a01c8f0edb28c3b12f039c478aa6b379bee81c8c
LimeShulkerBox <77601849+LimeShulkerBox@users.noreply.github.com>
2021-05-26 10:08:43 +1000

    Initial release

---
+++ src/main/java/com/limeshulkerbox/youreafishnow/CanBreatheInWaterMixinInitializer.java
@@ -0,0 +1,10 @@
+package com.limeshulkerbox.youreafishnow;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CanBreatheInWaterMixinInitializer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Multiplayer Button added!");
+    }
+}
---
+++ src/main/java/com/limeshulkerbox/youreafishnow/mixin/CanBreatheInWaterMixin.java
@@ -0,0 +1,42 @@
+package com.limeshulkerbox.youreafishnow.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(PlayerEntity.class)
+public abstract class CanBreatheInWaterMixin extends LivingEntity {
+	protected CanBreatheInWaterMixin(EntityType<? extends LivingEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@Override()
+	public boolean canBreatheInWater() {
+		return true;
+	}
+
+	@Unique
+	protected void tickWaterBreathingAir(int air) {
+		if (this.isAlive() && !this.isInsideWaterOrBubbleColumn()) {
+			this.setAir(air - 1);
+			if (this.getAir() == -20) {
+				this.setAir(0);
+				this.damage(DamageSource.DROWN, 2.0F);
+			}
+		} else {
+			this.setAir(300);
+		}
+
+	}
+
+	@Override
+	public void baseTick() {
+		int i = this.getAir();
+		super.baseTick();
+		this.tickWaterBreathingAir(i);
+	}
+}
